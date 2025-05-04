20e2118f27bf34e20b04b7802952067a688b0c12
Tarek Adam <tarek.adam@gmail.com>
2025-05-03 20:57:33 -0600

    deploy

--- service-worker.js
+++ service-worker.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', event => {
     );
 });
 
-const CACHE_NAME = 'cg-static-v7.8.61';
+const CACHE_NAME = 'cg-static-v7.8.62';
 const PRECACHE_URLS = [
     '/',
     '/index.html',
@@ -72,7 +72,7 @@ const PRECACHE_URLS = [
     '/manifest.json'
 ];
 
-console.log('🔥 SW loaded: version 7.8.61');
+console.log('🔥 SW loaded: version 7.8.62');
 
 self.addEventListener('install', event => {
     console.log('📦 Installing...');
